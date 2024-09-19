28ed6c49ac7a2054c5122681f93154315b9b368b
AnibalMaxDiaz <148412143+AnibalMaxDiaz@users.noreply.github.com>
2024-09-19 13:39:00 -0500

    add script for copy-button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // Next Project
 
 document.getElementById("card4").style.display = "none";
+let email = document.getElementById("email").innerText;
+let button = document.getElementById("copy-button")
+function copy(){
+  navigator.clipboard.writeText(email)
+}
